57217ff595a1365bafd868245145aa2464b76ec6
Mindaugas <ciamusu1@gmail.com>
2015-11-22 22:08:46 +0200

    minor fixes to inverse iteration

--- src/EigenvectorFinder/InverseIteration.java
+++ src/EigenvectorFinder/InverseIteration.java
@@ -2,7 +2,9 @@ package EigenvectorFinder;
 
 import org.apache.commons.math.linear.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Mindaugas on 11/20/15.
@@ -11,51 +13,66 @@ public class InverseIteration {
 
     private final double PRECISION = 0.0001;
 
-    //double[][] matrixData = { {5.64d,2d,0d,0d},{2d,5.64d,2d,0d}, {0d,2d,5.64d,2d},{0d, 0d,2d, 5.64d}};
-    double[][] matrixData = { {2d,-1d,0d,0d},{-1d,2d,-1d,0d}, {0d,-1d,2d,-1d},{0d, 0d,-1d, 2d}};
-    double[] initialEigenvector = {1,1,0,1};
+    double[][] matrixData = { {5.64d,2d,0d,0d},{2d,5.64d,2d,0d}, {0d,2d,5.64d,2d},{0d, 0d,2d, 5.64d}};
+    //double[][] matrixData = { {2d,-1d,0d,0d},{-1d,2d,-1d,0d}, {0d,-1d,2d,-1d},{0d, 0d,-1d, 2d}};
+    double[] initialEigenvector = {1d,1d,0d,1d};
     RealMatrix m = MatrixUtils.createRealMatrix(matrixData);
 
 
     public void solveInverseIteration(){
-        double eigenValueApproximation = 0;
+
+        double eigenValueApproximation = 1;
+
         double newEigenValueApprox = 0.0;
+        int iterationIndex = 0;
 
         RealVector previousXVector;
         RealVector xVector = MatrixUtils.createRealVector(initialEigenvector);
 
         do {
-            RealVector yVector = getYk(eigenValueApproximation, xVector);
-            eigenValueApproximation = newEigenValueApprox;
-
-            for(int i = 0; i < yVector.getData().length; i++){
-                System.out.println(yVector.getData()[i]);
-
+            iterationIndex++;
+            if(iterationIndex!=1){
+                eigenValueApproximation = newEigenValueApprox;
             }
 
-            previousXVector = xVector;
-            xVector = yVector.mapDivide(yVector.getNorm());
-            System.out.println("Eigenvector: "+ xVector);
+            try{
+                RealVector yVector = getYk(eigenValueApproximation, xVector);
+                System.out.println("Iteration number: " + iterationIndex);
+                previousXVector = xVector;
+                xVector = yVector.mapDivide(yVector.getNorm());
+                System.out.println("Eigenvector: "+ xVector);
+                for(int i = 0; i < xVector.getData().length; i++){
+                    System.out.println(xVector.getData()[i]);
+                }
 
-            double[] eigenValueVector = m.preMultiply(xVector).ebeMultiply(xVector).getData();
-            newEigenValueApprox = 0.0;
+                double[] eigenValueVector = m.preMultiply(xVector).ebeMultiply(xVector).getData();
+                newEigenValueApprox = 0.0;
 
-            for(int i = 0; i < eigenValueVector.length; i++){
-                newEigenValueApprox += eigenValueVector[i];
+                for(int i = 0; i < eigenValueVector.length; i++){
+                    newEigenValueApprox += eigenValueVector[i];
+                }
+
+                System.out.println("Eigenvalue = " + newEigenValueApprox + "\n");
+            } catch (SingularMatrixException e){
+                System.out.println("Choose another initial eigenvector");
+                break;
             }
-
-            System.out.println("Eigenvalue = " + newEigenValueApprox);
 
         } while(xVector.subtract(previousXVector).getNorm() > PRECISION && Math.abs(newEigenValueApprox-eigenValueApproximation) > PRECISION);
 
+        Double maxXVectorValue = Arrays.stream(xVector.getData()).map(i -> Math.abs(i)).max().getAsDouble();
+        List<Double> parametrizedXVector = Arrays.stream(xVector.getData()).map(notParametrized -> notParametrized / maxXVectorValue).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+
+        System.out.println("Parametrized results");
+        System.out.println(parametrizedXVector);
 
     }
 
-    public RealVector getYk(double eigenValueApproximation, RealVector xVector){
+    public RealVector getYk(double eigenValueApproximation, RealVector xVector) throws SingularMatrixException{
         return inverse(matrixMinusEigenvalue(eigenValueApproximation)).preMultiply(xVector);
     }
 
-    public RealMatrix inverse(RealMatrix matrixToInverse){
+    public RealMatrix inverse(RealMatrix matrixToInverse) throws SingularMatrixException{
         return new LUDecompositionImpl(matrixToInverse).getSolver().getInverse();
 
     }
